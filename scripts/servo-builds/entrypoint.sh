d920178330f8c0fafe45c3ad948d46e081a15a95
Benjamin Danis <benjamin.danis@dowjones.com>
2025-05-29 11:03:41 -0500

    cicd: test dynamic source

--- scripts/servo-builds/entrypoint.sh
+++ scripts/servo-builds/entrypoint.sh
@@ -5,37 +5,56 @@ set -euo pipefail
 # Main Program                                               #
 ##############################################################       
 
-# constants
-GITHUB_DOMAIN="github.com"
-GITHUB_OWNER="benjaminDanis"
-GITHUB_REPO="test-app-gen"
+GITHUB_DOMAIN=${GITHUB_DOMAIN_URL#https://}
+IFS='/' read -r GITHUB_OWNER GITHUB_REPO <<< "$GITHUB_REPOSITORY"
 
-# log the event payload
-echo "PUSH EVENT | Payload JSON:"
-echo "$EVENT_PAYLOAD" | jq '.'
+echo "GitHub Domain: $GITHUB_DOMAIN"
+echo "GitHub Owner: $GITHUB_OWNER"
+echo "GitHub Repo: $GITHUB_REPO"
 
 # create the servo path
-SERVO_PATH="/sources/$GITHUB_DOMAIN/$GITHUB_OWNER/$GITHUB_REPO/builds"
-echo "SERVO PATH: $SERVO_PATH"
+SERVO_API_BASE_URL="https://next.onservo.com/api"
+SERVO_API_REQUEST_PATH="/sources/$GITHUB_DOMAIN/$GITHUB_OWNER/$GITHUB_REPO/builds"
+echo "Servo API Request Path: $SERVO_API_REQUEST_PATH"
 
 # get servo parameters
 
-# get the commiit sha
+# get the commiit sha + branch
 COMMIT_SHA=$(echo "$EVENT_PAYLOAD" | jq -r '.head_commit.id')
-echo "COMMIT SHA: $COMMIT_SHA"
-
-# get branch
 BRANCH=$(echo "$EVENT_PAYLOAD" | jq -r '.ref' | sed 's|refs/heads/||')
-echo "BRANCH: $BRANCH"
+
+echo "Building SHA $COMMIT_SHA from branch $BRANCH"
+
+read -r -d '' PAYLOAD <<EOF
+{
+  "handle": "$COMMIT_SHA"
+}
+EOF
+
+# get builds to test URL
+RESPONSE=$(curl --location "${SERVO_API_BASE_URL}${SERVO_API_REQUEST_PATH}"
+  --request GET \
+  --header "token: $SERVO_TOKEN" | jq '.')
+
+echo "Servo Response: $RESPONSE"
 
 # test curl servo request
-SERVO_RESPONSE=$(curl --request POST \
-  --location "https://next.onservo.com/api/sources/$GITHUB_DOMAIN/$GITHUB_OWNER/$GITHUB_REPO/builds" \
-  --header "token: $SERVO_TOKEN" \
-  --header "Content-Type: application/json" \
-  --data-raw "{
-    \"handle\": \"$COMMIT_SHA\"
-  }" \
-  --silent | jq '.')
+# HTTP_RESPONSE=$(curl --write-out "HTTPSTATUS:%{http_code}" \
+#   --silent --show-error --request "$METHOD" \
+#   --location "https://next.onservo.com/api/$SERVO_API_REQUEST_PATH" \
+#   --header "token: $SERVO_TOKEN" \
+#   --header "Content-Type: application/json" \
+#   --data "$PAYLOAD")
 
-echo "SERVO RESPONSE: $SERVO_RESPONSE"
+# response data
+# HTTP_STATUS=$(echo "$HTTP_RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+# HTTP_BODY=$(echo "$HTTP_RESPONSE" | sed -e 's/HTTPSTATUS\:.*//g')
+
+# SERVO_RESPONSE_BODY=$(echo "$HTTP_BODY" | jq '.')
+# SERVO_RESPONSE_COMMIT_SHA=$(echo "$SERVO_RESPONSE" | jq -r '.commit.sha')
+# SERVO_RESPONSE_COMMIT_MESSAGE=$(echo "$SERVO_RESPONSE" | jq -r '.commit.message')
+# SERVO_RESPONSE_COMMIT_AUTHOR=$(echo "$SERVO_RESPONSE" | jq -r '.commit.author')
+# SERVO_RESPONSE_STATUS=$(echo "$SERVO_RESPONSE" | jq -r '.status')
+
+# Log and exit based on status
+# echo "$METHOD /api/sources/$GITHUB_DOMAIN/$GITHUB_OWNER/$GITHUB_REPO/builds status: $HTTP_STATUS"
